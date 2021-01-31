ccc75fcc199db134f69b3b33ae8af3d1309804e0
fhernand <feli.hp@gmail.com>
2021-02-01 00:30:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -66,6 +66,9 @@ class Circle {
       } else if (x >= this.width && y < this.width) {
         var index = (x-this.width) + (this.width-y-1)*this.width;
         this.map[i] = this.mapCircleQuarter1[index];
+      } else if (x < this.width && y >= this.width) {
+        var index = (this.width-x-1) + (y-this.width)*this.width;
+        this.map[i] = this.mapCircleQuarter1[index];
       }
     }
   }
