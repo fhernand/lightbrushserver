1b9a4c8132aff2471b079bb0a30a55236e31cb13
fhernand <feli.hp@gmail.com>
2021-01-31 22:53:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ class Circle {
     const x = Math.floor(i / (this.width*2));
     const y = i % (this.width*2);
     if (x >= this.width && y >= this.width){
-      return this.mapCircleQuarter[(x-this.width)+(y-this.width)];
+      return this.mapCircleQuarter[(x-this.width)*this.width - (y-this.width)];
     }
   }
   draw(n) {
