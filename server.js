c1f8e5fd0ba9804367c211da4ae1dd739821a756
fhernand <feli.hp@gmail.com>
2021-02-01 00:13:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ class Circle {
         this.map[i] = this.mapCircleQuarter[index]
         console.log(i);
       } else if (x < this.width && y < this.width) {
-        var transposed = his.mapCircleQuarter.map((_, i, a) => a[(i % this.width) * this.width + Math.floor(i / this.width)]);
+        var transposed = this.mapCircleQuarter.map((_, i, a) => a[(i % this.width) * this.width + Math.floor(i / this.width)]);
         this.map[i] = this.transposed[index];
       }
     }
