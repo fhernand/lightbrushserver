e1ea0b123de247d42ad4101e9a9c29cd551cdcdd
fhernand <feli.hp@gmail.com>
2021-01-31 21:46:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ class LedHandler {
     // to a custom Uint32Array to define your own map.
     this.config.width = NUM_LEDS_WIDTH;
     this.config.height = NUM_LEDS_HEIGHT;
-    this.config.map = 'alternating-matrix';
+    //this.config.map = 'alternating-matrix';
 
 
     // Configure ws281x
