9ca5920f15ed12072b5105fcc8e70362d29167f3
fhernand <feli.hp@gmail.com>
2021-01-31 22:31:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,34 +16,54 @@ app.use(express.static('public'));
 class Circle {
 
   constructor(width){
+
     this.width = width;
+    this.map = new Uint32Array(this.width * this.width);
+    this.mapCircleQuarter = new Uint32Array(this.width * this.width / 4);
     this.anteil = 0;
     this.granularity = 100;
   }
+  reset(){
+    new Uint32Array(this.width * this.width);
+  }
   setRadius(radius){
     this.radius = this.granularity*this.width*radius;
+    this.calculateMap();
+  }
+  calculateMap(){
+    for (var i = 0; i < this.width*this.width/4; i++) {
+      this.mapCircleQuarter[i] = draw(i);
+    }
+  }
+  getMapValue(i){
+    const x = Math.floor(i / this.width);
+    const y = i % this.width - 1;
+    if (x >= this.width && y >= this.width){
+      return this.mapCircleQuarter[(x-this.width)+(y-this.width)];
+    }
   }
   draw(n) {
     this.anteil = 0;
     const x = Math.floor(n / this.width);
-    const y = n % this.width;
+    const y = n % this.width - 1;
     const max_i = this.granularity*(x+1);
     const max_j = this.granularity*(y+1);
 
-    for (var i = max_i; i > this.granularity * x; i-- ){
-      for (var j = max_j; j > this.granularity * y; j-- ){
-        var dist_ij = Math.sqrt( (i * i) + (j * j) );
-        var value = dist_ij / this.radius;
-        if (i == max_i && j == max_j && value <= 1){
-          return 1;
-        } else {
-          if (value<=1){
-            this.anteil++;
+      for (var i = max_i; i > this.granularity * x; i-- ){
+        for (var j = max_j; j > this.granularity * y; j-- ){
+          var dist_ij = Math.sqrt( (i * i) + (j * j) );
+          var value = dist_ij / this.radius;
+          if (i == max_i && j == max_j && value <= 1){
+            return 1;
+
+          } else {
+            if (value<=1){
+              this.anteil++;
+            }
           }
         }
       }
-    }
-    return this.anteil / ( this.granularity * this.granularity );
+    return this.anteil / ( this.granularity * this.granularity );;
   }
 }
 
@@ -98,6 +118,7 @@ class LedHandler {
 
   setRadius(radius){
     this.circleInstance.setRadius(radius);
+    this.circleInstance.calculateMap();
   }
 
   loop() {
@@ -107,78 +128,79 @@ class LedHandler {
     var colorGreen = this.color.g * this.MaxBrightness / 255;
     var colorBlue = this.color.b * this.MaxBrightness / 255;
 
-      for (var i = 0; i < leds; i++) {
-        var value = this.circleInstance.draw(i);
-        var pixelColor = rgb2Int(value*colorRed,value*colorGreen,value*colorBlue);
-        pixels[i] = pixelColor;
-        // Set a specific pixel
-        //pixels[this.offset] = pixelColor;
+    this.circleInstance.reset();
+    for (var i = 0; i < leds; i++) {
+      var value = this.circleInstance.getMapValue(i);
+      var pixelColor = rgb2Int(value*colorRed,value*colorGreen,value*colorBlue);
+      pixels[i] = pixelColor;
+      // Set a specific pixel
+      //pixels[this.offset] = pixelColor;
 
-        // Move on to next
-        //this.offset = (this.offset + 1) % leds;
-      }
-      // Render to strip
-      ws281x.render(pixels);
+      // Move on to next
+      //this.offset = (this.offset + 1) % leds;
     }
-
-    run() {
-      setInterval(this.loop.bind(this), 100);
-    }
-  };
-
-  var ledHandlerInstance = new LedHandler();
-  ledHandlerInstance.setRadius(0.8);
-  var brightness = 50;
-  var color = { r:255, g:215, b:0 };
-
-  io.sockets.on('connection', (socket) => {
-    socket.emit('brightness', {value: brightness});
-
-    socket.on('brightness', function (data) { //makes the socket react to 'led' packets by calling this function
-      brightness = data.value;  //updates brightness from the data object
-      ledHandlerInstance.updateBrightness(brightness);
-      io.sockets.emit('brightness', {value: brightness}); //sends the updated brightness to all connected clients
-    });
-
-    socket.emit('red', {value: color.r});
-
-    socket.on('red', function (data) {
-      color.r = data.value;
-      ledHandlerInstance.updateColor(color);
-      io.sockets.emit('red', {value: color.r});
-    });
-
-    socket.emit('green', {value: color.g});
-
-    socket.on('green', function (data) {
-      color.g = data.value;
-      ledHandlerInstance.updateColor(color);
-      io.sockets.emit('green', {value: color.g});
-    });
-
-    socket.emit('blue', {value: color.b});
-
-    socket.on('blue', function (data) {
-      color.b = data.value;
-      ledHandlerInstance.updateColor(color);
-      io.sockets.emit('blue', {value: color.b});
-    });
-
-  });
-
-  ledHandlerInstance.run();
-
-  // ---- trap the SIGINT and reset before exit
-  process.on('SIGINT', function () {
-    ws281x.reset();
-    process.nextTick(function () { process.exit(0); });
-  });
-
-
-  function clearLEDs(){
-    ws281x.render(blackpixelData);
+    // Render to strip
+    ws281x.render(pixels);
   }
 
-  function rgb2Int(r, g, b) {
-    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+  run() {
+    setInterval(this.loop.bind(this), 100);
   }
+};
+
+var ledHandlerInstance = new LedHandler();
+ledHandlerIn  stance.setRadius(0.8);
+var brightness = 50;
+var color = { r:255, g:215, b:0 };
+
+io.sockets.on('connection', (socket) => {
+  socket.emit('brightness', {value: brightness});
+
+  socket.on('brightness', function (data) { //makes the socket react to 'led' packets by calling this function
+    brightness = data.value;  //updates brightness from the data object
+    ledHandlerInstance.updateBrightness(brightness);
+    io.sockets.emit('brightness', {value: brightness}); //sends the updated brightness to all connected clients
+  });
+
+  socket.emit('red', {value: color.r});
+
+  socket.on('red', function (data) {
+    color.r = data.value;
+    ledHandlerInstance.updateColor(color);
+    io.sockets.emit('red', {value: color.r});
+  });
+
+  socket.emit('green', {value: color.g});
+
+  socket.on('green', function (data) {
+    color.g = data.value;
+    ledHandlerInstance.updateColor(color);
+    io.sockets.emit('green', {value: color.g});
+  });
+
+  socket.emit('blue', {value: color.b});
+
+  socket.on('blue', function (data) {
+    color.b = data.value;
+    ledHandlerInstance.updateColor(color);
+    io.sockets.emit('blue', {value: color.b});
+  });
+
+});
+
+ledHandlerInstance.run();
+
+// ---- trap the SIGINT and reset before exit
+process.on('SIGINT', function () {
+  ws281x.reset();
+  process.nextTick(function () { process.exit(0); });
+});
+
+
+function clearLEDs(){
+  ws281x.render(blackpixelData);
+}
+
+function rgb2Int(r, g, b) {
+  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+}
