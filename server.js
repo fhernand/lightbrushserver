19061a79bceeb3ed72a92d48efbcc976fc8c1bdd
fhernand <feli.hp@gmail.com>
2021-02-07 23:08:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,56 +29,60 @@ io.sockets.on('connection', (socket) => {
     if(checkValue(data.value)!=null){
       brush = data.value
       ledHandlerInstance.setBrush(brush);
-        io.sockets.emit('brush', {value: brush});
-      }
-    });
-
-    socket.on('thumbslider', (data) => {
-      if(checkValue(data.value)!=null){
-        thumbslider = data.value;
-        ledHandlerInstance.updateThumbSlider(thumbslider);
-        io.sockets.emit('thumbslider', {value: thumbslider});
-      }
-    });
-
-    socket.on('pressure', function (data) {
-      if(checkValue(data.value)!=null){
-        pressure = data.value;
-        ledHandlerInstance.setPressure(pressure);
-        io.sockets.emit('pressure', {value: pressure});
-      }
-    });
-
-    socket.on('hexcolor', function (data) {
-      if(checkValue(data.value)!=null){
-        hexcolor = data.value;
-        ledHandlerInstance.updateHexColor(hexcolor);
-        io.sockets.emit('hexcolor', {value: hexcolor});
-      }
-    });
-
-    socket.on('error', function () {
-      io.sockets.emit('hexcolor', {value: '#ff0000'});
-      io.sockets.emit('thumbslider', {value: 255});
-      for (var i = 0; i < 3; i++)
-        blink();
-    });
+      io.sockets.emit('brush', {value: brush});
+    }
   });
 
-  function checkValue(value){
-    if (value!=null){
-      return value;
-    } else {
-      socket.emit('error');
-      return null;
+  socket.on('thumbslider', (data) => {
+    if(checkValue(data.value)!=null){
+      thumbslider = data.value;
+      ledHandlerInstance.updateThumbSlider(thumbslider);
+      io.sockets.emit('thumbslider', {value: thumbslider});
     }
-  }
+  });
 
-  function blink(){
-    for(i=0; i<= 100;i++){
-      o.sockets.emit('pressure', {value: i});
+  socket.on('pressure', function (data) {
+    if(checkValue(data.value)!=null){
+      pressure = data.value;
+      ledHandlerInstance.setPressure(pressure);
+      io.sockets.emit('pressure', {value: pressure});
     }
-    for(i=99; i>= 0;i--){
-      o.sockets.emit('pressure', {value: i});
+  });
+
+  socket.on('hexcolor', function (data) {
+    if(checkValue(data.value)!=null){
+      hexcolor = data.value;
+      ledHandlerInstance.updateHexColor(hexcolor);
+      io.sockets.emit('hexcolor', {value: hexcolor});
     }
+  });
+
+  socket.on('error', function () {
+    io.sockets.emit('hexcolor', {value: '#ff0000'});
+    io.sockets.emit('thumbslider', {value: 255});
+    for (var i = 0; i < 3; i++){
+      blink();
+    }
+    socket.emit('thumbslider', {value: thumbslider});
+    socket.emit('pressure', {value: pressure});
+    socket.emit('hexcolor', {value: hexcolor});
+  });
+});
+
+function checkValue(value){
+  if (value!=null){
+    return value;
+  } else {
+    socket.emit('error');
+    return null;
   }
+}
+
+function blink(){
+  for(i=0; i<= 100;i++){
+    o.sockets.emit('pressure', {value: i});
+  }
+  for(i=99; i>= 0;i--){
+    o.sockets.emit('pressure', {value: i});
+  }
+}
