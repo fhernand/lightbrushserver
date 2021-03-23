984e07da312e593c47a0cc0b309fbeff358fa4c8
fhernand <fhernand@users.noreply.github.com>
2021-03-23 22:49:57 +0100

    Update leddriver.js

--- leddriver.js
+++ leddriver.js
@@ -15,7 +15,7 @@ class LEDDriver {
   }
   
   clearPixels(){
-    for (var i = 0; i <= this.width*this.height; i++){
+    for (var i = 0; i < this.width*this.height; i++){
       this.setPixel(i, 0, 0, 0); 
     }
     this.showPixels();
